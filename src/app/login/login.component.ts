9159db5792fc48db5d1ad8d4b93974dd89e0adc9
sarathckm <sarathckmckm@gmail.com>
2022-07-25 11:11:05 +0530

    template reference

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
   acno=''
   pswd=''
 
-  userDetails={
+  userDetails:any={
     1000:{acno:1000,username:'Neer',password:1000,balance:5000},
     1001:{acno:1001,username:'Laisha',password:1001,balance:4000},
     1002:{acno:1002,username:'Neer',password:1002,balance:6000}
@@ -23,9 +23,9 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  login(){
-    alert("login clicked...")
-  }
+  // login(){
+  //   alert("login clicked...")
+  // }
   acnoChange(event:any){
     this.acno=event.target.value
     console.log(this.acno);
@@ -36,5 +36,43 @@ export class LoginComponent implements OnInit {
     console.log(this.pswd);
     
   }
+  // login(){
+  //   var acno=this.acno
+  //   var pswd=this.pswd
+  //   let userDetails=this.userDetails
 
-}
+  //   if(acno in userDetails){
+  //     if(pswd==userDetails[acno]['password']){
+  //       alert('Login Successful')
+  //     }
+  //     else{
+  //       alert("incorrect password")
+  //     }
+  //     }
+  //     else{
+  //       alert("user does not exist")
+  //     }
+  //   }
+  // login with 2 args
+    login(a:any,p:any){
+      var acno=a.value
+      var pswd=p.value
+      let userDetails=this.userDetails
+  
+      if(acno in userDetails){
+        
+        if(pswd == userDetails[acno].password){
+          alert('Login Successful')
+        }
+        else{
+          alert("incorrect password")
+        }
+        }
+        else{
+          alert("user does not exist")
+        }
+      }
+  }
+
+
+
